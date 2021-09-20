d694f8613b776d8063cc260441acd7c56ba796de
ctvidic <chrisvidic.cv@gmail.com>
2021-09-20 17:35:14 -0400

    profileFetch

--- frontend/src/components/profile/profile.js
+++ frontend/src/components/profile/profile.js
@@ -22,6 +22,7 @@ class Profile extends React.Component {
     }
 
     render() {
+        debugger;
             return (
                 <div>
                     <h2>All of This User's Tweets</h2>
