b4bf4b68be1f6e99458b91eb8e818b07ce2e1f77
Michael Amicucci <mikeamicucci34@gmail.com>
2021-09-21 12:15:04 -0400

    aws

--- app.js
+++ app.js
@@ -6,8 +6,14 @@ const mongoose = require('mongoose');
 const passport = require('passport');
 
 const users = require("./routes/api/users");
+const artworks = require('./routes/api/artwork');
 // const tweets = require("./routes/api/tweets");
 const path = require('path');
+const bodyParser = require('body-parser');
+require('dotenv').config({ path: './frontend/env' }) //dpible check correct path
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 mongoose
   .connect(db, { useNewUrlParser: true })
@@ -22,6 +28,7 @@ require('./config/passport')(passport);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/api/users", users);
+app.use("/api/artworks", artworks);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'));
@@ -31,3 +38,4 @@ if (process.env.NODE_ENV === 'production') {
 }
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
--- routes/api/artwork.js
+++ routes/api/artwork.js
@@ -2,9 +2,13 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const passport = require('passport');
+const multer = require('multer')           
+const Aws = require('aws-sdk')                
+// require('dotenv').config({ path: '../.env' }) //dpible check correct path
+require('dotenv').config() //dpible check correct path
 const validateArtworkCreate = require('../../validation/artwork_create');
 const Artwork = require('../../models/Artwork'); 
-const { default: artworks } = require('../../frontend/src/components/artwork/artworks');
+// const { default: artworks } = require('../../frontend/src/components/artwork/artworks');
 
 
 router.get("/", (req, res) => {
@@ -31,23 +35,106 @@ router.get("/:id", (req, res) => {
 });
 
 
-router.post('/',
-    passport.authenticate('jwt', { session: false }),
-    (req, res) => {
-      const { errors, isValid } = validateArtworkCreate(req.body);
+// router.post('/',
+//     passport.authenticate('jwt', { session: false }),
+//     (req, res) => {
+//       const { errors, isValid } = validateArtworkCreate(req.body);
   
-      if (!isValid) {
-        return res.status(400).json(errors);
-      }
+//       if (!isValid) {
+//         return res.status(400).json(errors);
+//       }
   
-      const newArtwork = new Artwork({
-        title: req.body.title,
-        // user: req.user.id,
-        description: req.body.description,
-        price: req.body.price
+//       const newArtwork = new Artwork({
+//         title: req.body.title,
+//         // user: req.user.id,
+//         description: req.body.description,
+//         price: req.body.price
         
-      });
+//       });
   
-      newArtwork.save().then(artwork => res.json(artwork));
+//       newArtwork.save().then(artwork => res.json(artwork));
+//     }
+//   );
+
+  // attempt to post artwork 
+
+const storage = multer.memoryStorage({
+    destination: function (req, file, cb) {
+        cb(null, '')
     }
-  );
+})
+
+const filefilter = (req, file, cb) => {
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/jpg') {
+        cb(null, true)
+    } else {
+        cb(null, false)
+    }
+}
+
+const upload = multer({ storage: storage, fileFilter: filefilter });
+// const upload = multer({ storage: storage });
+
+const s3 = new Aws.S3({
+    accessKeyId:process.env.AWS_ACCESS_KEY_ID,              
+    secretAccessKey:process.env.AWS_ACCESS_KEY_SECRET       
+})
+
+router.post('/', upload.single('artworkimage'), (req, res) => {
+    console.log(req.file)
+    console.log(req.body)
+
+    const params = {
+        Bucket: process.env.AWS_BUCKET_NAME,      // bucket that we made earlier
+        Key: req.file.originalname,               // Name of the image
+        Body: req.file.buffer,                    // Body which will contain the image in buffer format
+        ACL: "public-read-write",                 // defining the permissions to get the public link
+        ContentType: "image/jpeg"                 // Necessary to define the image content-type to view the photo in the browser with the link
+    };
+
+    s3.upload(params,(error,data)=>{
+        if(error){
+            res.status(500).send({"err":error})  // if we get any error while uploading error message will be returned.
+        }
+
+            console.log(data)                      // this will give the information about the object in which photo is stored 
+     
+    const artwork = new Artwork({
+            title: req.body.title,
+            description: req.body.description,
+            price: req.body.price,
+            artworkImage: data.Location
+        });
+        artwork.save()
+            .then(result => {
+                res.status(200).send({
+                    _id: result._id,
+                    title: result.title,
+                    description: result.description,
+                    price: result.price,
+                    artworkImage: data.Location,
+                })
+            })
+            .catch(err => {
+                res.send({ message: err })
+          })
+    })
+})
+
+router.get('/', (req, res) => {
+    try {
+        console.log("hello")
+        // const artworks = await Artwork.find()
+        // const artworks = Artwork.find().then((artworks) => res.send(artworks))
+        Artwork.find()
+          .then((artworks) => res.send(artworks))
+        
+        // console.log(artworks)
+        // res.send(artworks)
+    } catch (err) {
+        res.send({ message: err, m:"not working" })
+    }
+});
+
+module.exports = router
+
