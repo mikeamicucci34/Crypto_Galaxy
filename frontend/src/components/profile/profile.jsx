ab1b157434747dd55614cfaec1b05d9fe16cf1ca
omarluq <omaralanii@outlook.com>
2021-09-20 21:21:47 -0700

    live updates on user profile

--- frontend/src/components/profile/profile.jsx
+++ frontend/src/components/profile/profile.jsx
@@ -4,28 +4,23 @@ import './profile.css'
 class Profile extends React.Component {
     constructor(props) {
         super(props);
-
         this.state = {
-            tweets: [],
-            bio: ''
-        }
-
-        this.submitBio = this.submitBio.bind(this)
-    }
-
-    componentWillMount() {
-        if (this.props.currentUser){
-        console.log(this.props.currentUser.id)
+            user: this.props.user,
+            toggle: "show"
         }
     }
+
+    
     componentDidMount(){
         if (this.props.currentUser){
         this.props.fetchUser(this.props.currentUser.id);
         }
     }
-    componentWillReceiveProps(newState) {
- 
+
+    componentDidUpdate(){
+        this.props.fetchUser(this.props.currentUser.id)
     }
+    
     submitBio(e){
         e.preventDefault();
         let user= {
@@ -34,8 +29,9 @@ class Profile extends React.Component {
             bio: this.state.bio,
             id: this.props.user._id
         }
-        debugger;
         this.props.updateUser(user)
+        
+        this.setState({toggle: 'show'})
         
     }
 
@@ -45,15 +41,25 @@ class Profile extends React.Component {
         }
     }
     render() {
+        if (!this.props.user) return null;
+        console.log(this.state.toggle);
+        let bio;
+        if(this.state.toggle === "show"){
+            bio = <div>
+                        <div>{this.props.user.bio}</div>
+                        <button onClick={()=>this.setState({toggle: 'edit'})}>Edit Bio</button>
+                    </div>
+        } else if(this.state.toggle === "edit") {
+            bio = <form onSubmit={(e)=>this.submitBio(e)}>
+                        <textarea placeholder='Biography' onChange={this.update('bio')}></textarea>
+                        <button>Submit</button>
+                  </form>
+        }
         
             return (
                 <div id="midCard">
                     <h1>Username: {this.props.user.handle}</h1>
-                    <form onSubmit={this.submitBio}>
-                        <textarea placeholder='Biography' onChange={this.update('bio')}></textarea>
-                        <button>Submit</button>
-                    </form>
-                    <div>asdfasdfasdf: {this.props.user.bio}</div>
+                    {bio}
                 </div>
             );
         
