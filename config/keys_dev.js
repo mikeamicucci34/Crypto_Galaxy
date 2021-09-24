d5578ffc84db036d26fb19f017bb690d793bd4a8
ctvidic <chrisvidic.cv@gmail.com>
2021-09-24 09:29:49 -0400

    update

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,5 +1,8 @@
 module.exports = {
     // mongoURI: 'mongodb://dev:lPS7ewogmFmAkvxM@cluster.mongodb.net/test?retryWrites=true&w=majority'
     mongoURI: 'mongodb+srv://dev:lPS7ewogmFmAkvxM@cluster0.tbegs.mongodb.net/test?retryWrites=true&w=majority',
-    secretOrKey: 'secret'
+    secretOrKey: 'secret',
+    awsBucketName: "crpto-galaxy",
+    awsAccessKeySecret: "9SGECxvPqgChi+gL1dycwrb8CEJnkZOM8zFjwQNO",
+    awsAccessKeyId: "AKIA3Y2HMJRB66VSUOYO"
 }
--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,7 @@
 module.exports = {
     mongoURI: process.env.MONGO_URI,
-    secretOrKey: process.env.SECRET_OR_KEY
+    secretOrKey: process.env.SECRET_OR_KEY,
+    awsBucketName: process.env.AWS_BUCKET_NAME,
+    awsAcessKeySecret: process.env.AWS_ACCESS_KEY_SECRET,
+    awsAccessKeyId=prcoess.env.AWS_ACCESS_KEY_ID
 }
