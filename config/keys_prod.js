fcf8ca900310e10fd405106a9c9c685a07f54480
ctvidic <chrisvidic.cv@gmail.com>
2021-09-24 10:22:12 -0400

    update

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -2,6 +2,7 @@ module.exports = {
     mongoURI: process.env.MONGO_URI,
     secretOrKey: process.env.SECRET_OR_KEY,
     awsBucketName: process.env.AWS_BUCKET_NAME,
-    awsAccessKeySecret: process.env.AWS_ACCESS_KEY_SECRET,
+    awsAccessKeySecret: process.env.AWS_SECRET_ACCESS_KEY,
     awsAccessKeyId: process.env.AWS_ACCESS_KEY_ID
+    
 }
