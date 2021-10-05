590ba98549fbd72580d6e8cbafbe2223eeff558c
Michael Amicucci <mikeamicucci34@gmail.com>
2021-10-04 21:54:29 -0400

    description formatting

--- frontend/src/components/profile/artworkcard.jsx
+++ frontend/src/components/profile/artworkcard.jsx
@@ -19,6 +19,24 @@ class ArtworkCard extends React.Component {
 
 
     render(){
+
+         // limit description length on profile to 100 characters
+
+        let truncateDescription;
+        if (this.state.description.length >= 110) {
+           truncateDescription = this.state.description.slice(0,110) + "..."
+        } else {
+            truncateDescription = this.state.description
+        }
+
+        let truncatetitle;
+        if (this.state.title.length >= 20) {
+           truncatetitle = this.state.title.slice(0,20) + "..."
+        } else {
+            truncatetitle = this.state.title
+        }
+
+
         return (
             <Link className="outer-link-card" to={`/artworks/${this.state._id}`}>
                 <motion.div
@@ -27,9 +45,9 @@ class ArtworkCard extends React.Component {
                 >
                         <Link className="nft-link" to={`/artworks/${this.state._id}`}><img className="nft" src={this.state.artworkImage} alt="" /></Link>
                         <div className="title-like">
-                        <h3 className="title">{this.state.title}</h3>
+                        <h3 className="profile_cardTitle">{truncatetitle}</h3>
                         </div>
-                        <h3>{this.state.description}</h3>
+                        <h3 className="cardDescription">{truncateDescription}</h3>
                         <Link to={`./update_artworks/${this.state.artworkId}`}></Link>
                 </motion.div>
             </Link>
